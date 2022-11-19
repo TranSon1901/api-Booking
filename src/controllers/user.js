165899a2213acf8861752584260cf8a595338368
transon <transon1901@gmail.com>
2022-11-19 09:56:41 +0700

    add

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,37 +2,37 @@ import User from "../models/User.js";
 
 const getAllUser = async (req,res)=>{
    try{
-
+     const user = await User.find()
+     res.status(200).json(user)
    } catch(err){
      next(err)
    }
 } 
 const getUser = async (req,res)=>{
     try{
-
-    } catch(err){
-      next(err)
-    }
-} 
-const createUser = async (req,res)=>{
-    try{
-
+      const user = await User.findById(req.params.id)
+      res.status(200).json(user)
     } catch(err){
       next(err)
     }
 } 
 const deleteUser = async (req,res)=>{
     try{
-
+       await User.findByIdAndDelete(req.params.id)
+       res.status(200).json('delete user')
     } catch(err){
       next(err)
     }
 } 
 const updateUser = async (req,res)=>{
     try{
-
+       const updateUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {$set : req.body},
+        {new:true})
+        res.status(200).json(updateUser)
     } catch(err){
       next(err)
     }
 } 
-export {getAllUser,getUser,deleteUser,updateUser,createUser}
+export {getAllUser,getUser,deleteUser,updateUser}
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,9 @@
 import express from 'express'
-import { createUser, deleteUser, getAllUser, getUser, updateUser } from '../controllers/user.js'
+import { deleteUser, getAllUser, getUser, updateUser } from '../controllers/user.js'
 
 const router=express.Router()
 router.get('/',getAllUser)
-router.get('/',getUser)
-router.get('/',createUser)
+router.get('/:id',getUser)
 router.get('/',deleteUser)
 router.get('/',updateUser)
 export default router
