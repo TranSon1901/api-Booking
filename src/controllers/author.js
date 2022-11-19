fcf99c539ab68c82d2f2d9e55dc83dfb25384b96
transon <transon1901@gmail.com>
2022-11-19 09:19:02 +0700

    add

--- src/controllers/author.js
+++ src/controllers/author.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js"
-import bcrypt from 'bcryptjs'
+import bcrypt from "bcryptjs"
+import { createError } from "../utils/error.js";
 const register = async (req,res,next)=>{
     try{
        const salt = bcrypt.genSaltSync(10);
@@ -15,4 +16,18 @@ const register = async (req,res,next)=>{
       next(err)
    }
 }
-export {register}
+const login = async (req,res,next) =>{
+    try{
+      const user = await User.findOne({username:req.body.username})
+      if(!user) return next(createError(404,'not found user'))
+
+      const isPassWordCorret= await bcrypt.compareSync(req.body.password, user.password);
+      if(!isPassWordCorret) return next(createError(400,'error password'))
+      
+      const {password,isAdmin,...otherDetails}= user._doc
+      res.status(200).json(otherDetails)
+    } catch(err){
+        next(err)
+    }
+}
+export {register,login}
